e173f5aa7f1777db7c503a62b0cda382e275fc4c
Q <88193155+rizaisyah@users.noreply.github.com>
2023-07-20 12:11:15 +0700

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+# Judul dan sub judul
+st.title("QUALITAS AIR SUNGAI CODE")
+st.subheader("Visualisasi data yang ditampilkan berdasarkan titik sample yang diambil di Kota Jogja")
